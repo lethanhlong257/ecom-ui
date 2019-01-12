99d7fac05a34678bd38f4590cabbeedaeacc907d
Long <lethanhlong257@gmail.com>
2019-01-13 01:17:58 +0700

    Add Material UI and Tim-creative UI KIT to project

--- ecom-ui/README.md
+++ ecom-ui/README.md
@@ -1,5 +1,8 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+Project us Marterials UI - developed by Gooogle
+https://material-ui.com/getting-started/installation/
+
 ## Available Scripts
 
 In the project directory, you can run:
---
+++ ecom-ui/src/routes/index.jsx
@@ -0,0 +1,13 @@
+import LandingPage from "views/LandingPage/LandingPage.jsx";
+import ProfilePage from "views/ProfilePage/ProfilePage.jsx";
+import LoginPage from "views/LoginPage/LoginPage.jsx";
+import App from "views/App.jsx";
+
+var indexRoutes = [
+  { path: "/landing-page", name: "LandingPage", component: LandingPage },
+  { path: "/profile-page", name: "ProfilePage", component: ProfilePage },
+  { path: "/login-page", name: "LoginPage", component: LoginPage },
+  { path: "/", name: "App", component: App }
+];
+
+export default indexRoutes;
